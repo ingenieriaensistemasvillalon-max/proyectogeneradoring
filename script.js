b62b81ec34772362572c6ce73e7a94e6d17b6640
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-09-26 16:35:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ const musica = [
   "Amore.",
   "El vals del minuto.",
   "Angels.",
-];
+]
 // Definimos la función que mostrará una cancion aleatoria
 function generarMusica() {
   // Generamos un número aleatorio entre 0 y la cantidad de musicas (musica.length)
   // Math.random() → número entre 0 y 1
-  // * citas.length → escala ese número al rango de musicas disponibles
+  // * musica.length → escala ese número al rango de musicas disponibles
   // Math.floor() → redondea hacia abajo para obtener un número entero válido
   const indice = Math.floor(Math.random() * musica.length);
 
@@ -27,6 +27,7 @@ function generarMusica() {
   // Cambiamos su contenido de texto por la musica que corresponde al índice generado
   document.getElementById("quote").innerText = musica[indice];
  }
+
 
 
 
