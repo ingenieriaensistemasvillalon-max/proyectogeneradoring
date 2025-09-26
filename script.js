94d6ce5f90299595eaf64e9579d2f270694759ec
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-09-26 14:45:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // Declaramos un arreglo (array) con varias frases motivacionales
 const musica = [
-  "El éxito es la suma de pequeños esfuerzos repetidos día tras día.",
-  "Nunca es tarde para aprender algo nuevo.",
-  "El futuro pertenece a quienes creen en la belleza de sus sueños.",
-  "La programación no se aprende en un día, se construye con paciencia.",
-  "No te rindas, el inicio siempre es lo más difícil.",
-  "Cada error es una oportunidad para mejorar.",
-  "El conocimiento es poder, pero la práctica lo transforma en sabiduría."
+  "River Flows In You.",
+  "Can't Help Falling in Love.",
+  "My Heart Will Go On.",
+  "Imagine.",
+  "Married Life.",
+  "Comptine d'un Autre été: L'Après-Midi.",
+  "Your Song."
+  "Love of My Life."
+  "Here, There and Everywhere."
+  "Hallelujah."
+  "Shallow."
+  "Amore."
+  "El vals del minuto."
+  "Angels."
 ];
 
 // Definimos la función que mostrará una cita aleatoria
@@ -21,4 +28,5 @@ function generarMusica() {
   // Cambiamos su contenido de texto por la musica que corresponde al índice generado
   document.getElementById("quote").innerText = musica[indice];
 }
+
 
