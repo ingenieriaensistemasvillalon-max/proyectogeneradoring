6b4ffc290b97685074438b0c44550c3c0f09399f
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-09-26 14:29:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Declaramos un arreglo (array) con varias frases motivacionales
-const citas = [
+const musica = [
   "El éxito es la suma de pequeños esfuerzos repetidos día tras día.",
   "Nunca es tarde para aprender algo nuevo.",
   "El futuro pertenece a quienes creen en la belleza de sus sueños.",
@@ -10,14 +10,15 @@ const citas = [
 ];
 
 // Definimos la función que mostrará una cita aleatoria
-function generarCita() {
-  // Generamos un número aleatorio entre 0 y la cantidad de frases (citas.length)
+function generarMusica() {
+  // Generamos un número aleatorio entre 0 y la cantidad de musicas (musica.length)
   // Math.random() → número entre 0 y 1
-  // * citas.length → escala ese número al rango de frases disponibles
+  // * citas.length → escala ese número al rango de musicas disponibles
   // Math.floor() → redondea hacia abajo para obtener un número entero válido
-  const indice = Math.floor(Math.random() * citas.length);
+  const indice = Math.floor(Math.random() * musica.length);
 
   // Buscamos en el documento el elemento con id="quote"
-  // Cambiamos su contenido de texto por la frase que corresponde al índice generado
-  document.getElementById("quote").innerText = citas[indice];
+  // Cambiamos su contenido de texto por la musica que corresponde al índice generado
+  document.getElementById("quote").innerText = musica[indice];
 }
+
