1d03422edbadcf50a29c0369bc148c16d5740e35
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-09-26 17:02:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const musica = [
   "My Heart Will Go On.",
   "Imagine.",
   "Married Life.",
-  "Comptine d un Autre été:,
- "Your Song",
+  "Comptine d'un Autre été:
+  L'Après-Midi.",
+  "Your Song.",
   "Love of My Life.",
   "Here, There and Everywhere.",
   "Hallelujah.",
@@ -15,18 +16,19 @@ const musica = [
   "El vals del minuto.",
   "Angels.",
 ];
+
 // Definimos la función que mostrará una cancion aleatoria
 function generarMusica() {
   // Generamos un número aleatorio entre 0 y la cantidad de musicas (musica.length)
   // Math.random() → número entre 0 y 1
-  // * musica.length → escala ese número al rango de musicas disponibles
+  // * citas.length → escala ese número al rango de musicas disponibles
   // Math.floor() → redondea hacia abajo para obtener un número entero válido
   const indice = Math.floor(Math.random() * musica.length);
 
   // Buscamos en el documento el elemento con id="quote"
   // Cambiamos su contenido de texto por la musica que corresponde al índice generado
   document.getElementById("quote").innerText = musica[indice];
- }
+}
 
 
 
